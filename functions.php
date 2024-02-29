b4b157e3f586fa2036e000c98dc0ded70a84b1d3
tko971 <tiekko971@gmail.com>
2024-02-29 10:23:06 +0100

    Premier commit

---
+++ functions.php
@@ -0,0 +1,21 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'theme-style', get_stylesheet_directory_uri() . '/assets/css/focechild.css' );
+    wp_enqueue_script( 'focechildjs', get_stylesheet_directory_uri() . '/assets/js/focechild.js', array(), time(), true );
+
+}
+
+
+
+// Get customizer options form parent theme
+if ( get_stylesheet() !== get_template() ) {
+    add_filter( 'pre_update_option_theme_mods_' . get_stylesheet(), function ( $value, $old_value ) {
+        update_option( 'theme_mods_' . get_template(), $value );
+        return $old_value; // prevent update to child theme mods
+    }, 10, 2 );
+    add_filter( 'pre_option_theme_mods_' . get_stylesheet(), function ( $default ) {
+        return get_option( 'theme_mods_' . get_template(), $default );
+    } );
+}
